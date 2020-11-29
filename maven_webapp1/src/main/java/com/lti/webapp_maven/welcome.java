b9e31e6b92b5f872567c39b81b62fe19756ddcc7
saadshaikh <saadshaikh462@hotmail.com>
2020-11-29 12:04:36 +0530

    commit1

---
+++ maven_webapp1/src/main/java/com/lti/webapp_maven/welcome.java
@@ -0,0 +1,19 @@
+package com.lti.webapp_maven;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class welcome {
+
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello REST Api";
+	}
+}
